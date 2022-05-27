1f2362ef8a24aac9880d5b566d03d3acabbf76af
aged777 <222volkov@gmail.com>
2022-05-27 12:00:54 +0300

    task JAva pre-project 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+
+    private static String urlDB = "jdbc:mysql://localhost:3306/mydbstudy";
+    private static String userName  = "root1";
+    private static String password = "root";
+
+    public static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        }
+
+        try {
+            connection = DriverManager.getConnection(urlDB, userName, password);
+        } catch (SQLException e) {
+            System.out.println("Ошибка SQL при создании подключения!");;
+        }
+    }
+
+
     // реализуйте настройку соеденения с БД
 }
